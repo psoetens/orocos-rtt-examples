bd9bd08aaf84be10f44d3dd313dfd1714181f68a
Peter Soetens <peter@thesourceworks.com>
2010-02-07 13:41:56 +0100

    exercices: complete hello-6-scripting exercise.

--- rtt-exercises/hello-6-scripting/HelloWorld.cpp
+++ rtt-exercises/hello-6-scripting/HelloWorld.cpp
@@ -19,6 +19,7 @@
 
 #include <boost/lambda/lambda.hpp>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 using namespace boost;
@@ -30,7 +31,7 @@ using namespace Orocos;
  *
  * In this exercise, we will write some basic Orocos program scripts and
  * state machines. The details for the exercise are in program.ops and
- * statemachine.osd. This file contains the ready-to-be-used components
+ * statemachine.osd. This file contains the almost-ready-to-be-used components
  * in which our scripts will run.
  *
  * Optional 1: Modify Hello and World such that they load the program.ops
@@ -66,26 +67,58 @@ namespace Example
             return v;
         }
 
+        // useful for debugging our scripts.
+        void say(const string& who, const string& message) {
+            cout <<' '<<who<<" :" <<message << endl;
+        }
+
         /** @} */
 
         InputPort<Data> input;
         OutputPort<Data> output;
 
+        Property<int>    max_data;
+        Property<double> gain;
+        Property<Data>   gains;
     public:
         /**
-         * This example sets the interface up in the Constructor
-         * of the component.
+         * We initialize the ports, the operations and the properties.
          */
         Hello(std::string name)
-            : TaskContext(name), input("input"), output("output")
+            : TaskContext(name),
+              input("input"), output("output", true), // keeps last written value.
+              max_data("max_data","The maximum number of elements in our Data struct.", 10),
+              gain("gain","A single gain.",1.0),
+              gains("gains","A vector of gains",Data(max_data.get(),1.0))
         {
-            this->provides()->addOperation("multiply", &Hello::multiply, this).doc("Multiplies.")
+            addOperation("multiply", &Hello::multiply, this).doc("Multiplies.")
                     .arg("v","Data").arg("f", "Factor");
-            this->provides()->addOperation("divide", &Hello::multiply, this).doc("Divides.")
+            addOperation("divide", &Hello::multiply, this).doc("Divides.")
                     .arg("v","Data").arg("f", "Factor");
+            addOperation("say", &Hello::say, this).doc("Logs to cout.")
+                    .arg("sender","Who's saying it.").arg("message","The message.");
 
-            this->ports()->addPort(&input);
-            this->ports()->addPort(&output);
+            ports()->addPort(&input);
+            ports()->addPort(&output);
+
+            addProperty(&gain);
+            addProperty(&gains);
+            addProperty(&max_data);
+        }
+
+        bool configureHook() {
+            if (max_data.get() < 1 ) {
+                log(Error) <<"Invalid number of max data elements."<<endlog();
+                return false;
+            }
+            // resizing gains property
+            Data new_sample = gains.get();
+            new_sample.resize(max_data.get(),gain.get());
+            gains.set( new_sample );
+
+            // setting output port
+            output.setDataSample( gains.get() );
+            return true;
         }
     };
 
