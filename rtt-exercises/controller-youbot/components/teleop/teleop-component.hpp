0080acde992a095a1836e2e56ff46a9a2fabee99
Peter Soetens <peter@thesourceworks.com>
2012-04-10 23:45:58 +0200

    controller-youbot: add untested teleop 'demo' code.
    Signed-off-by: Peter Soetens <peter@thesourceworks.com>

--- rtt-exercises/controller-youbot/components/teleop/teleop-component.hpp
+++ rtt-exercises/controller-youbot/components/teleop/teleop-component.hpp
@@ -12,13 +12,16 @@ class Teleop
     int linear_x, linear_y;
     int angular_z;
     double l_scale_, a_scale_;
+    bool do_demo;
+    int demo_counter;
     RTT::InputPort<sensor_msgs::Joy> joystick;
     RTT::OutputPort<geometry_msgs::Twist> cmdvel;
  public:
     Teleop(string const& name)
         : TaskContext(name),
           linear_x(1),linear_y(0),
-          angular_z(2), l_scale_(1), a_scale_(1)
+          angular_z(2), l_scale_(1), a_scale_(1), 
+          do_demo(false), demo_counter(0)
     {
         provides()->doc("A component which takes any USB joystick and translates it to teleoperation commands for the Youbot. The default axes are configured for the PlayStation 3 joystick.");
 
@@ -33,15 +36,20 @@ class Teleop
 
         addProperty("l_scale",l_scale_).doc("Scales the linear axes to a linear velocity.");
         addProperty("a_scale",a_scale_).doc("Scales the angular axis to an angular velocity.");
+
+        addOperation("demo", &Teleop::demo, this).arg("on_off", "Turn the demo mode on (true) or off (false)" ).doc("Turns on or off 'demo' mode, which allows you to simulate a joystick in case you have no access to one.");
     }
 
-    bool configureHook() {
-        std::cout << "Teleop configured !" <<std::endl;
-        return true;
+
+    void demo( bool on_off ) {
+        do_demo = on_off;
+        this->trigger(); // start updateHook()
     }
 
     bool startHook() {
-        std::cout << "Teleop started !" <<std::endl;
+        geometry_msgs::Twist vel;
+        cmdvel.write(vel);
+        demo_counter = 0;
         return true;
     }
 
@@ -53,14 +61,31 @@ class Teleop
             vel.linear.y = l_scale_ * joy.axes[linear_y];
             vel.angular.z = a_scale_ * joy.axes[angular_z];
             cmdvel.write(vel);
+        } else {
+            if (do_demo) {
+                demo_counter++;
+                if (demo_counter == 100)
+                    vel.linear.x = l_scale_ * 1;
+                else if (demo_counter < 200)
+                    vel.linear.z = a_scale_ * 1;
+                else if (demo_counter < 300)
+                    vel.linear.y = l_scale_ * 1;
+                else
+                    demo_counter = 0;
+                // hacked sleep-and-trigger logic. RTT should offer this out of the box !
+                cmdvel.write(vel);
+                TIME_SPEC ts;
+                ts.tv_sec = 0;
+                ts.tv_nsec = 1000*1000*20; // 20ms
+                rtos_nanosleep(&ts, 0);
+                this->trigger();
+            }
         }
     }
 
     void stopHook() {
-    }
-
-    void cleanupHook() {
-        std::cout << "Teleop cleaning up !" <<std::endl;
+        geometry_msgs::Twist vel;
+        cmdvel.write(vel);
     }
 };
 
