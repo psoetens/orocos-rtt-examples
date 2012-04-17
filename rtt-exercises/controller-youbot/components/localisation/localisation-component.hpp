488aba0b16c8efcd9770f4962692e3dda12dc75b
Peter Soetens <peter@intermodalics.eu>
2012-04-17 07:44:28 +0200

    added extra operations exercise.
    Signed-off-by: Peter Soetens <peter@intermodalics.eu>

--- rtt-exercises/controller-youbot/components/localisation/localisation-component.hpp
+++ rtt-exercises/controller-youbot/components/localisation/localisation-component.hpp
@@ -10,7 +10,6 @@
 class Localisation
     : public RTT::TaskContext
 {
-    RTT::OperationCaller<geometry_msgs::TransformStamped (const std::string&, const std::string& )> getPose;
     geometry_msgs::TransformStamped tfs;
     RTT::OutputPort<geometry_msgs::Pose2D> outpose;
  public:
@@ -21,15 +20,13 @@ class Localisation
     }
 
     bool configureHook() {
-        if ( getPeer("tf") == 0 )
-            return false;
-        getPose = getPeer("tf")->getOperation("lookupTransform");
-        return getPose.ready();
+        /** Check in here that we can call rtt_tf */
+        return false;
     }
 
     void updateHook() {
         try {
-            tfs = getPose("/odom","/base_footprint");
+            /* tfs = ...call rtt_tf and query the transform /odom to /base_footprint ... ; */
         } catch (...) {
             return;
         }
